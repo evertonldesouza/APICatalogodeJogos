5f72daa05dfa088fb95534737df229eb24f5aecb
Everton L. de Souza <evertonldesouza@outlook.com>
2021-07-17 20:11:01 -0300

    First commit

---
+++ APICatalogodeJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogodeJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogodeJogos.Repositories
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# APICatalogodeJogos
- API  exibindo um catalogo de jogos
+# API Catalogo de Jogos
+Desenvolvido atraves das aulas do Curso de NET da Digital Inoovation One
+
+## Previews
+
+![preview 01](./APICatalogodeJogos/preview/preview1.png)
+
+
+# Tecnologias
+
+- NET C#
